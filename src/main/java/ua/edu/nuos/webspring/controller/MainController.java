1ed242faed1fb440ae16ff1fa10d9ff875c8a645
eugenyberkut <eugeny.berkunsky@gmail.com>
2016-10-11 13:05:58 +0300

    Initial commit

---
+++ src/main/java/ua/edu/nuos/webspring/controller/MainController.java
@@ -0,0 +1,11 @@
+package ua.edu.nuos.webspring.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Eugeny on 11.10.2016.
+ */
+@Controller
+public class MainController {
+    
+}
